6b24184175dfea40b089b1982e66cbdad80e77ee
Christoph Schnabl <christoph.schnabl.cs@gmail.com>
2020-04-06 18:48:11 +0200

    Add transfer savings goal happy path tests

--- src/test/kotlin/xyz/schnabl/RoundupIntegrationTest.kt
+++ src/test/kotlin/xyz/schnabl/RoundupIntegrationTest.kt
@@ -1,29 +1,32 @@
 package xyz.schnabl
 
-import com.google.inject.AbstractModule
-import com.google.inject.Guice
-import com.google.inject.Injector
-import com.google.inject.Provides
-import com.google.inject.name.Named
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.ResponseBody
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import okio.Buffer
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import xyz.schnabl.remote.AmountDto
+import xyz.schnabl.remote.GsonSerdeService
 import xyz.schnabl.remote.StarlingClient
 import xyz.schnabl.remote.account.AccountDto
-import xyz.schnabl.remote.account.AccountsDto
 import xyz.schnabl.remote.feed.FeedItemDto
 import xyz.schnabl.remote.feed.TransactionDirection
 import xyz.schnabl.remote.feed.TransactionFeedDto
+import xyz.schnabl.remote.savings.CreateSavingsGoalDto
+import xyz.schnabl.remote.savings.CreateSavingsTransferDto
+import xyz.schnabl.remote.savings.SavingsGoalDto
+import xyz.schnabl.remote.savings.TransferSavingsGoalDto
 import java.time.LocalDateTime
 import java.time.ZoneOffset
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.test.assertEquals
@@ -35,23 +38,26 @@ class RoundupIntegrationTest {
     private val GBP = Currency.getInstance("GBP")
     private val accountUid = UUID.fromString("a7ab343e-ef3e-4e39-a850-2d0c5bb44cde")
     private val defaultCategory = UUID.fromString("0791a3a1-487c-4998-b11b-3852dc833ed3")
+    private val savingGoalsUid = UUID.fromString("303f02e2-90af-4444-a0a1-cec0469b9207")
     private val expectedAccount = AccountDto(accountUid, defaultCategory, GBP, parseTime("2020-03-22T19:33:48.990Z"))
 
-    private lateinit var injector: Injector
+    private val gson = GsonBuilder().registerTypeAdapter(
+        LocalDateTime::class.java,
+        JsonDeserializer { json: JsonElement, _, _ ->
+            ZonedDateTime.parse(
+                json.asJsonPrimitive.asString
+            ).toLocalDateTime()
+        } as JsonDeserializer<LocalDateTime>
+    ).create()
+
     private lateinit var client : StarlingClient
 
     @Before
     fun setUp() {
         server.start()
-
-        injector = Guice.createInjector(object : AbstractModule() {
-            @Provides
-            fun provideConfig(): Config {
-                return Config("http://${server.hostName}:${server.port}", "", "", "", "", "", "")
-            }
-        })
-
-        client = injector.getInstance(StarlingClient::class.java)
+        // TODO load config dynamically
+        val config = Config("http://${server.hostName}:${server.port}/api/v2", "accounts", "feed", "category", "savings-goals", "account", "add-money")
+        client = StarlingClient(OkHttpClient(), config, GsonSerdeService(gson))
     }
 
     @After
@@ -62,6 +68,7 @@ class RoundupIntegrationTest {
     @Test
     fun `testGETAccountsForUserHappyPath`() {
         val expected = listOf(expectedAccount)
+        val expectedUrl = server.url("api/v2/accounts")
 
         // Given
         server.enqueue(responseBodyFromJson("account.json"))
@@ -69,8 +76,11 @@ class RoundupIntegrationTest {
         // When
         val actual = client.getAccountsForUser()
 
+        val recordedRequest = server.takeRequest()
+
         // Then
         assertEquals(expected, actual)
+        assertEquals(expectedUrl, recordedRequest.requestUrl)
     }
 
 
@@ -91,14 +101,61 @@ class RoundupIntegrationTest {
         val feedItem2Amount = AmountDto(GBP, 10)
         val feedItem2 = FeedItemDto(feedItem2Uid, defaultCategory, feedItem2Amount, TransactionDirection.OUT, transactionTime2, settlementTime2)
 
+        val changesSince = LocalDateTime.now()
         val expectedFeed = TransactionFeedDto(listOf(feedItem1, feedItem2))
-
+        val expectedUrl = server.url("api/v2/feed/account/$accountUid/category/$defaultCategory?changesSince=${changesSince.toInstant(ZoneOffset.UTC)}")
 
         server.enqueue(responseBodyFromJson("feed.json"))
 
-        val actualFeed = client.getTransactionsForAccountByCategory(expectedAccount.accountUid, expectedAccount.defaultCategory, LocalDateTime.now())
+        val actualFeed = client.getTransactionsForAccountByCategory(expectedAccount.accountUid, expectedAccount.defaultCategory, changesSince)
+
+        val recordedRequest = server.takeRequest()
 
         assertEquals(expectedFeed, actualFeed)
+        assertEquals(expectedUrl, recordedRequest.requestUrl)
+    }
+
+    @Test
+    fun `testPUTSavingsGoalHappyPath`() {
+        val expectedUrl = server.url("api/v2/account/$accountUid/savings-goals")
+        val expectedSavingsGoal = SavingsGoalDto(savingGoalsUid, true, listOf())
+        val expectedCreateSavingsGoalDto = CreateSavingsGoalDto("Hansi", GBP, AmountDto(GBP, 4242))
+
+        server.enqueue(responseBodyFromJson("savingsGoal.json"))
+
+        val savingsGoal = client.createSavingsGoal(accountUid, "Hansi", 4242)
+
+        val request = server.takeRequest()
+
+        // CHECK RequestBody
+        assertEquals(gson.toJson(expectedCreateSavingsGoalDto), request.body.toBody())
+
+        // CHECK RequestUrl
+        assertEquals(expectedUrl, request.requestUrl)
+
+        // CHECK Response
+        assertEquals(expectedSavingsGoal, savingsGoal)
+    }
+
+    @Test
+    fun `testPUTTransferToSavingsGoalHappyPath`() {
+        val transferUid = UUID.fromString("5d04b20b-4109-4dbe-b261-6729ba72c504")
+        val expectedCreateSavingsTransfer = CreateSavingsTransferDto(AmountDto(GBP, 1337))
+        val expectedSavingsTransfer = TransferSavingsGoalDto(transferUid, true, listOf())
+
+        server.enqueue(responseBodyFromJson("transferSavingsGoal.json"))
+        val savingsTransfer = client.transferToSavingsGoal(accountUid, savingGoalsUid, transferUid, 1337)
+
+        val recordedRequest = server.takeRequest()
+
+        // CHECK RequestBody
+        assertEquals(gson.toJson(expectedCreateSavingsTransfer), recordedRequest.body.toBody())
+
+        // CHECK Url
+        val expectedUrl = server.url("api/v2/account/$accountUid/savings-goals/$savingGoalsUid/add-money/$transferUid")
+        assertEquals(expectedUrl, recordedRequest.requestUrl)
+
+        assertEquals(expectedSavingsTransfer, savingsTransfer)
     }
 
 
@@ -116,5 +173,9 @@ class RoundupIntegrationTest {
 
     private fun parseTime(localDateTime: String): LocalDateTime {
         return LocalDateTime.parse(localDateTime, DateTimeFormatter.ISO_ZONED_DATE_TIME)
+    }
+
+    private fun Buffer.toBody() : String {
+        return String(inputStream().readAllBytes())
     }
 }
